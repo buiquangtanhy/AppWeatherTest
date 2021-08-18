1ac7abc99274762581c27579b20cf92c7cabfbda
buiquangtanhy <tanhy411998@gmail.com>
2021-08-18 23:06:57 +0700

    Update main.dart
    Fix UI

--- lib/main.dart
+++ lib/main.dart
@@ -60,20 +60,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter')),
+      // appBar: AppBar(title: const Text('Counter')),
       body: BlocProvider(
         create: (context) => WeatherBloc(dataService: dataService)
           ..add(SearchEventRequested(city: 'Vietnam')),
         // _tfCity.text.trim()
-        child: _Screen(context),
+        child: _ScreenWeather(context),
       ),
     );
   }
 
 // =============================================
 
-  _Screen(BuildContext context) {
+  _ScreenWeather(BuildContext context) {
     return Container(
+      color: Colors.grey[350],
       child: Column(
         children: [
           BlocBuilder<WeatherBloc, WeatherState>(builder: (context, state) {
@@ -84,10 +85,10 @@ class _MyHomePageState extends State<MyHomePage> {
             if (state is WeatherStateLoaded) {
               return Container(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    _Search(context),
+                    _SearchCity(context),
                     _buildWeatherNow(state.weatherResponse),
                     _buildWeatherDays(state.weatherResponseDays),
                   ],
@@ -104,16 +105,19 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  _Search(BuildContext context) {
+  _SearchCity(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(5.0),
+      padding: EdgeInsets.all(10.0),
       child: TextField(
         controller: _tfCity,
         style: TextStyle(color: Colors.black),
         decoration: InputDecoration(
             hintText: 'Search City',
             icon: Icon(Icons.search),
-            hintStyle: TextStyle(color: Colors.blueGrey)),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32.0)),
+            ),
+            hintStyle: TextStyle(color: Colors.grey)),
         onEditingComplete: () async {
           BlocProvider.of<WeatherBloc>(context)
               .add(SearchEventRequested(city: _tfCity.text.trim()));
@@ -124,6 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _buildWeatherNow(WeatherResponse weatherResponse) {
     return Container(
+      height: 250,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -136,12 +141,15 @@ class _MyHomePageState extends State<MyHomePage> {
               fontWeight: FontWeight.bold,
             ),
           ),
+          Padding(padding: EdgeInsets.only(top: 5)),
           Text(
             'Humidity: ${weatherResponse.tempInfo.humidity}%',
             style: TextStyle(fontSize: 20, color: Colors.blue),
           ),
-          Text('Description: ' + weatherResponse.weatherInfo.description),
-          Text(weatherResponse.cityName),
+          Padding(padding: EdgeInsets.only(top: 5)),
+          Text(weatherResponse.weatherInfo.description.toUpperCase()),
+          Padding(padding: EdgeInsets.only(top: 5)),
+          Text(weatherResponse.cityName.toUpperCase(), style: TextStyle(color: Colors.deepOrange, fontSize: 20),),
         ],
       ),
     );
@@ -149,8 +157,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _buildWeatherDays(WeatherResponseDays weatherResponseDays) {
     return Container(
+      alignment: Alignment.bottomCenter,
       padding: EdgeInsets.only(top: 10),
-      height: 200.0,
+      height: 210.0,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
           shrinkWrap: true,
@@ -161,78 +170,69 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  // _buildWeatherDays(List<Weather> weatherResponseDays) {
-  //   return Container(
-  //     height: 200.0,
-  //     child: ListView.builder(
-  //         scrollDirection: Axis.horizontal,
-  //         shrinkWrap: true,
-  //         itemCount: weatherResponseDays.length,
-  //         itemBuilder: (context, index) {
-  //           return _buildWeatherDay(weatherResponseDays[index]);
-  //         }),
-  //   );
-  // }
-  // _buildWeatherDay(Weather weather1day) {
-  //
-  //   return Container(
-  //     padding: EdgeInsets.all(5),
-  //     child: Column(
-  //       children: [
-  //         Image.network(weather1day.iconUrl),
-  //         Row(
-  //           children: [
-  //             Text(
-  //               '${weather1day.description}°C',
-  //               style: TextStyle(color: Colors.redAccent, fontSize: 10),
-  //             ),
-  //             Text(
-  //               '${weather1day.main}%',
-  //               style: TextStyle(color: Colors.blue, fontSize: 10),
-  //             )
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
   _buildWeatherDay(Weather1Day weather1day) {
     DateTime a = DateTime.fromMicrosecondsSinceEpoch(weather1day.day * 1000);
     int day = a.day;
     int month = a.month;
     int year = a.year;
+    int b = a.weekday;
+
     return Container(
-      padding: EdgeInsets.all(5),
+      width: 130,
+      height: 210,
+      padding: EdgeInsets.all(3),
       child: Card(
-
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(color: Colors.white, width: 3),
+        ),
         color: Colors.lightGreenAccent,
-        shadowColor: Colors.lightGreen,
-        elevation: 10,
         child: Column(
-
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Padding(padding: EdgeInsets.only(top: 6)),
+            // Padding(padding: EdgeInsets.only(top: 10)),
             Text(
               '$day/$month/$year',
-              style: TextStyle(color: Colors.black54, fontSize: 14,),
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w900),
             ),
-            Image.network(weather1day.iconUrl),
+            Container(
+              height: 80,
+              width: 80,
+              alignment: Alignment.center,
+              child: Image.network(
+                weather1day.iconUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Text(
-                  '${weather1day.teamInfo.day}°C - ',
-                  style: TextStyle(color: Colors.redAccent, fontSize: 15,fontWeight: FontWeight.w600),
+                  '${weather1day.teamInfo.day}°C',
+                  style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600),
                 ),
                 Text(
                   '${weather1day.humidity}%',
-                  style: TextStyle(color: Colors.blue, fontSize: 15,fontWeight: FontWeight.w600),
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600),
                 )
               ],
             ),
             Text(
-              '${weather1day.weathers.description}',
-              style: TextStyle(color: Colors.black, fontSize: 13, fontWeight: FontWeight.w600),
+              '${weather1day.weathers.description.toUpperCase()}',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600),
             ),
           ],
         ),
